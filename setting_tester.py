3779ae13eb928f0390eb257fdab97211b2899cdd
evg-cv <evgenijkolotov333@gmail.com>
2021-04-11 16:22:10 +0300

    Mutli cam read

--- setting_tester.py
+++ setting_tester.py
@@ -7,5 +7,5 @@ from settings import COLLECTION_PATH
 image_utils = ImageUtils()
 image = cv2.imread(COLLECTION_PATH)
 processed_image = image_utils.run(frame=image)
-cv2.imshow("Processed Image", cv2.resize(processed_image, (1600, 1200)))
+cv2.imshow("Processed Image", cv2.resize(processed_image, None, fx=0.4, fy=0.4))
 cv2.waitKey()
