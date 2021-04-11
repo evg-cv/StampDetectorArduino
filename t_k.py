1696a2f298efb2d9a14dffe7775b42a02508cd48
evg-cv <evgenijkolotov333@gmail.com>
2021-04-11 13:15:27 +0300

    Mutli cam read

--- t_k.py
+++ t_k.py
@@ -50,25 +50,25 @@ class CamThread:
 
 
 def display_cam_view():
-    # cap1 = cv2.VideoCapture(0)
+    cap1 = cv2.VideoCapture(0)
     # cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
     # cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
     cap2 = cv2.VideoCapture(1)
-    # cap3 = cv2.VideoCapture(2)
+    cap3 = cv2.VideoCapture(2)
     while True:
-        # _, frame1 = cap1.read()
+        _, frame1 = cap1.read()
         # print(frame1.shape[:2])
         _, frame2 = cap2.read()
-        # _, frame3 = cap3.read()
+        _, frame3 = cap3.read()
         # print(frame3.shape[:2])
-        # cv2.imshow("Frame1", frame1)
+        cv2.imshow("Frame1", frame1)
         cv2.imshow("Frame2", frame2)
-        # cv2.imshow("Frame3", frame3)
+        cv2.imshow("Frame3", frame3)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # cap1.release()
+    cap1.release()
     cap2.release()
-    # cap3.release()
+    cap3.release()
     cv2.destroyAllWindows()
 
 
