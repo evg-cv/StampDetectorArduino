850d0b8aa5f58a21516d46a8b563bbee5e115660
evg-cv <evgenijkolotov333@gmail.com>
2021-04-11 12:59:26 +0300

    Mutli cam read

--- t_k.py
+++ t_k.py
@@ -1,25 +1,53 @@
+import threading
 import time
 import cv2
 
-cap1 = cv2.VideoCapture(0)
-time.sleep(0.1)
-# cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
-# cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
-cap2 = cv2.VideoCapture(1)
-time.sleep(0.1)
-cap3 = cv2.VideoCapture(2)
-while True:
-    _, frame1 = cap1.read()
-    # print(frame1.shape[:2])
-    _, frame2 = cap2.read()
-    _, frame3 = cap3.read()
-    # print(frame3.shape[:2])
-    cv2.imshow("Frame1", frame1)
-    cv2.imshow("Frame2", frame2)
-    cv2.imshow("Frame3", frame3)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap1.release()
-cap2.release()
-cap3.release()
-cv2.destroyAllWindows()
+
+class CamThread:
+    def __init__(self):
+        self.top_frame = None
+        self.bottom_frame = None
+        self.stamp_detector_frame = None
+        self.break_ret = False
+
+    def get_frame(self, num_port, cam_cat):
+        cap = cv2.VideoCapture(num_port)
+        while True:
+            if self.break_ret:
+                cap.release()
+                break
+            _, frame = cap.read()
+            if cam_cat == "top":
+                self.top_frame = frame
+            elif cam_cat == "bottom":
+                self.bottom_frame = frame
+            elif cam_cat == "stamp":
+                self.stamp_detector_frame = frame
+            time.sleep(0.02)
+
+        return
+
+    def run(self):
+        top_frame_thread = threading.Thread(target=self.get_frame, args=[0, "top"])
+        bottom_frame_thread = threading.Thread(target=self.get_frame, args=[1, "bottom"])
+        stamp_frame_thread = threading.Thread(target=self.get_frame, args=[2, "stamp"])
+        top_frame_thread.start()
+        bottom_frame_thread.start()
+        stamp_frame_thread.start()
+        while True:
+            cv2.imshow("Top Frame", self.top_frame)
+            cv2.imshow("Bottom Frame", self.bottom_frame)
+            cv2.imshow("Stamp Detector Frame", self.stamp_detector_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.break_ret = True
+                top_frame_thread.join()
+                bottom_frame_thread.join()
+                stamp_frame_thread.join()
+                break
+        cv2.destroyAllWindows()
+
+        return
+
+
+if __name__ == '__main__':
+    CamThread().run()
