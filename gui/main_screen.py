b45d3b0522982a0e0b1201f1b43da1d274d2f588
evg-cv <evgenijkolotov333@gmail.com>
2021-05-14 23:52:33 +0300

    Stop issue

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -201,14 +201,14 @@ class MainScreen(Screen):
 
     def stop_process(self):
         pic_per_collection = int(self.ids.pic_per_collection.text)
-        self.start_ret = False
-        self.ard_com.receive_ret = False
-        if self.run_time_threading is not None:
-            self.run_time_threading.join()
-        if self.ard_threading is not None:
-            self.ard_threading.join()
-        if self.main_threading is not None:
-            self.main_threading.join()
+        # self.start_ret = False
+        # self.ard_com.receive_ret = False
+        # if self.run_time_threading is not None:
+        #     self.run_time_threading.join()
+        # if self.ard_threading is not None:
+        #     self.ard_threading.join()
+        # if self.main_threading is not None:
+        #     self.main_threading.join()
         self.processing_time = 0
         self.stamp_num = 0
         self.picture_num = 1
