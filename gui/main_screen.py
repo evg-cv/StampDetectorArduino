a00338e3b46dd56e37eb6c31b443aa72ab112952
evg-cv <evgenijkolotov333@gmail.com>
2021-05-15 01:40:52 +0300

    Image load issue

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -183,7 +183,7 @@ class MainScreen(Screen):
     @mainthread
     def insert_image(self, rotate_img_path, align_img_path):
         self.ids.rotated_image.source = rotate_img_path
-        self.ids.align_image.source = align_img_path
+        self.ids.align_image.source = rotate_img_path
         self.ids.finished_collection.text = str(self.finished_collection)
         self.ids.no_stamps.text = str(self.stamp_num)
         # print(self.rotated_image_path)
